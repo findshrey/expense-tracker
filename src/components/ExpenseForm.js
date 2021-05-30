c808214ec5d21b7c5192005bb953cca591552be2
shrey9496 <shrey9april@gmail.com>
2021-05-31 01:58:20 +0530

    Feat: Add expense type option and summry

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,39 +6,55 @@ const ExpenseForm = () => {
    const [title, setTitle] = useState('')
    const [amount, setAmount] = useState('')
    const [date, setDate] = useState('')
+   const [expenseType, setExpenseType] = useState('income')
 
    const { handleAddExpense } = useContext(ExpenseContext)
 
    const handleTitle = (e) => setTitle(e.target.value)
    const handleAmount = (e) => setAmount(e.target.value)
    const handleDateChange = (e) => setDate(e.target.value)
+   const handleExpenseType = (e) => setExpenseType(e.target.value)
 
    const handleSubmit = (e) => {
       e.preventDefault()
 
+      // Check for income or expense
+      const signedAmount = expenseType === 'expense' ? -amount : +amount
+
       const expenseData = {
          id: Math.random(),
          title: title,
-         amount: amount,
+         amount: signedAmount,
          date: new Date(date)
       }
 
       handleAddExpense(expenseData)
+
+      setTitle('')
+      setAmount('')
+      setDate('')
    }
 
    return (
       <form onSubmit={handleSubmit}>
          <div className="form-control">
-            <label htmlFor="title">Title</label>
-            <input id="title" value={title} onChange={handleTitle} type="text" />
+            <label >Title</label>
+            <input type="text" value={title} onChange={handleTitle} />
          </div>
          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input id="amount" value={amount} onChange={handleAmount} type="number" />
+            <label>Amount</label>
+            <input type="number" min="0" value={amount} onChange={handleAmount} />
          </div>
          <div className="form-control">
-            <label htmlFor="date">Date</label>
-            <input type="date" min='2020-01-01' value={date} onChange={handleDateChange} />
+            <label>Date</label>
+            <input type="date" min="2020-01-01" value={date} onChange={handleDateChange} />
+         </div>
+         <div className="form-control">
+            <label>Expense Type</label>
+            <select value={expenseType} onChange={handleExpenseType}>
+               <option value="income">Income</option>
+               <option value="expense">Expense</option>
+            </select>
          </div>
          <div className="expense-actions">
             <button type="submit">Add Expense</button>
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,9 +5,14 @@ import ExpenseContext from './../context/ExpenseContext'
 const Summary = () => {
    const { expenses } = useContext(ExpenseContext)
 
+   const totalBalance = expenses.reduce((total, expense) => {
+      return total + expense.amount
+   }, 0)
+
    return (
       <section>
-         <p>Summary</p>
+         <h2>Current Balance</h2>
+         <div>{totalBalance}</div>
       </section>
    )
 }
