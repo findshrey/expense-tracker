dbf86863a6eb15cf54e40b9fed8db5faf20be389
shrey9496 <shrey9april@gmail.com>
2021-05-31 14:01:10 +0530

    Feat: expense list filter option

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,17 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 
 import ExpenseContext from './../context/ExpenseContext'
 import ExpenseItem from './ExpenseItem'
 
+const filterExpenses = (expenses, filterBy) => {
+   if (filterBy === 'income') {
+      return expenses.filter((expense) => expense.amount > 0)
+   } else if (filterBy === 'expense') {
+      return expenses.filter((expense) => expense.amount < 0)
+   } else {
+      return expenses
+   }
+}
+
 const ExpenseList = () => {
+   const [filterBy, setFilterBy] = useState('all')
    const { expenses, handleRemoveExpense } = useContext(ExpenseContext)
+
+   const handleFilter = (option) => setFilterBy(option)
+
+   const renderedExpenses = filterExpenses(expenses, filterBy)
 
    return (
       <section className="expense-list">
          <h2>All Transactions</h2>
+         <div className="expense-list-actions">
+            <button onClick={() => handleFilter('all')}>All</button>
+            <button onClick={() => handleFilter('income')}>Income</button>
+            <button onClick={() => handleFilter('expense')}>Expense</button>
+         </div>
          <ul className="expense-list-inner">
             {
-               expenses.map((expense) => (
+               renderedExpenses.map((expense) => (
                   <ExpenseItem
                      key={expense.id}
                      expense={expense}
