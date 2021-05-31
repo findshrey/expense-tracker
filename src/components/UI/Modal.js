e32b5c818005348b0f137440598f9e885cd05333
shrey9496 <shrey9april@gmail.com>
2021-06-01 03:10:57 +0530

    Feat: react portal for modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 const Modal = ({ handleRemoveExpense, handleModal }) => {
-   return (
-      <div className="modal">
-         <header>
-            <h2>Remove transaction?</h2>
-         </header>
-         <div className="modal-content">
-            <p>Are you sure you want to remove this transaction?</p>
+   return ReactDOM.createPortal(
+      <>
+         <div className="modal-backdrop" />
+         <div className="modal">
+            <header>
+               <h2>Remove transaction?</h2>
+            </header>
+            <div className="modal-content">
+               <p>Are you sure you want to remove this transaction?</p>
+            </div>
+            <footer>
+               <button onClick={handleRemoveExpense}>Yes</button>
+               <button onClick={handleModal}>No</button>
+            </footer>
          </div>
-         <footer>
-            <button onClick={handleRemoveExpense}>Yes</button>
-            <button onClick={handleModal}>No</button>
-         </footer>
-      </div>
+      </>,
+      document.getElementById('modal-root')
    )
 }
 
